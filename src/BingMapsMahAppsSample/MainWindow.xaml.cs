0879c9202838b86771e0c3c35feca577405c853c
Jan Karger <punkerat76@gmail.com>
2015-10-09 17:05:35 +0200

    just a login test

--- src/BingMapsMahAppsSample/MainWindow.xaml.cs
+++ src/BingMapsMahAppsSample/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Threading;
 using MahApps.Metro.Controls;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Maps.MapControl.WPF;
 
 namespace BingMapsMahAppsSample
 {
@@ -11,6 +16,31 @@ namespace BingMapsMahAppsSample
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => DialogHost.IsOpen = true));
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs e)
+        {
+            if ((bool)e.Parameter)
+            {
+                //System.Net.WebRequest.DefaultWebProxy = new WebProxy("", 80);
+                //System.Net.WebRequest.DefaultWebProxy.Credentials = new NetworkCredential("", "");
+                Map.CredentialsProvider = new ApplicationIdCredentialsProvider(AppIdTextBox.Text);
+                Map.Mode = new RoadMode();
+            }
+            else
+            {
+                Application.Current.Shutdown(0);
+            }
         }
     }
 }
