3dbd9268edb1a9518f3fc6c9f645c9b07c634ac7
Jan Karger <punkerat76@gmail.com>
2015-10-09 12:52:24 +0200

    first raw empty stuff, using paket

---
+++ src/BingMapsMahAppsSample/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MahApps.Metro.Controls;
+
+namespace BingMapsMahAppsSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
