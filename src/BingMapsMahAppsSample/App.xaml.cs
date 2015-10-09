1ccada412e5c855e9576482e2b14d6736078bc6e
Jan Karger <punkerat76@gmail.com>
2015-10-09 17:05:10 +0200

    add MaterialDesignThemes

--- src/BingMapsMahAppsSample/App.xaml.cs
+++ src/BingMapsMahAppsSample/App.xaml.cs
@@ -8,5 +8,9 @@ namespace BingMapsMahAppsSample
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 }
